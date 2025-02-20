1eb948d77a3bae9aecb0deec7a1b08cca9e34046
hennequin <gje.hennequin@gmail.com>
2025-02-20 19:44:21 +0000

    LQR tested: OK

--- examples/lgs_from_scratch.ml
+++ examples/lgs_from_scratch.ml
@@ -20,21 +20,42 @@ let base =
     }
 
 let m = 10
+let n = 20
 let o = 40
+let tmax = 130
 let bs = 32
 let id_m = Maths.(const (Tensor.of_bigarray ~device:base.device (Owl.Mat.eye m)))
 let ones_o = Maths.(const (Tensor.ones ~device:base.device ~kind:base.kind [ o ]))
 let ones_u = Maths.(const (Tensor.ones ~device:base.device ~kind:base.kind [ m ]))
 
-let make_c ~sigma (m, o) =
+let make_a () =
+  let a =
+    let open Owl in
+    let w =
+      let w = Mat.(gaussian n n) in
+      let sa = Linalg.D.eigvals w |> Dense.Matrix.Z.re |> Mat.max' in
+      Mat.(Float.(0.8 / sa) $* w)
+    in
+    (*     let w = Mat.(0.5 $* w + transpose w) in *)
+    Mat.(add_diag (0.1 $* w) 0.9)
+  in
+  Tensor.of_bigarray ~device:base.device a |> Maths.const
+
+let make_b () =
   Tensor.(
-    f Float.(sigma /. sqrt (of_int m))
-    * randn ~device:base.device ~kind:base.kind [ m; o ])
+    f Float.(1. /. sqrt (of_int m)) * randn ~device:base.device ~kind:base.kind [ m; n ])
+  |> Maths.const
+
+let make_c () =
+  Tensor.(
+    f Float.(1. /. sqrt (of_int n)) * randn ~device:base.device ~kind:base.kind [ n; o ])
   |> Maths.const
 
 module PP = struct
   type 'a p =
-    { c : 'a
+    { a : 'a
+    ; b : 'a
+    ; c : 'a
     ; sigma_o_prms : 'a
     }
   [@@deriving prms]
@@ -43,29 +64,22 @@ end
 module P = PP.Make (Prms.P)
 
 let true_theta =
-  let sigma_o_prms = Maths.(log (f 0.01)) in
-  let c = make_c ~sigma:1. (m, o) in
-  PP.{ sigma_o_prms; c }
+  let a = make_a () in
+  let b = make_b () in
+  let c = make_c () in
+  let sigma_o_prms = Maths.(log (f 0.001)) in
+  PP.{ a; b; c; sigma_o_prms }
 
 let theta =
-  let c = Prms.free (Maths.primal (make_c ~sigma:1. (m, o))) in
+  let a = Prms.free (Maths.primal (make_a ())) in
+  let b = Prms.free (Maths.primal (make_b ())) in
+  let c = Prms.free (Maths.primal (make_c ())) in
   let sigma_o_prms =
     Prms.create
       ~below:(Tensor.f 5.)
       Tensor.(f 0. + zeros ~device:base.device ~kind:base.kind [ 1 ])
   in
-  PP.{ c; sigma_o_prms }
-
-let sample_data =
-  let sigma_o = Maths.(exp true_theta.sigma_o_prms) in
-  let c = true_theta.c in
-  fun () ->
-    let us =
-      Tensor.(randn ~device:base.device ~kind:base.kind [ bs; m ]) |> Maths.const
-    in
-    let xs = Maths.(us *@ c) in
-    let ys = Maths.(xs + (sigma_o * const (Tensor.randn_like (primal xs)))) in
-    us, ys
+  PP.{ a; b; c; sigma_o_prms }
 
 let solver a y =
   let ell = Maths.cholesky a in
@@ -73,15 +87,78 @@ let solver a y =
   let _x = Maths.linsolve_triangular ~left:false ~upper:true ell_t y in
   Maths.linsolve_triangular ~left:false ~upper:false ell _x
 
-let posterior_precision_chol ~c ~inv_sigma_o =
-  let r = Maths.(sqr inv_sigma_o) in
-  Maths.(id_m + einsum [ c, "ij"; r, "j"; c, "kj" ] "ik") |> Maths.cholesky
+let lqr ~a ~b ~c ~inv_sigma_o y =
+  let open Maths in
+  (* augment observations with dummy y0 *)
+  let _T = List.length y in
+  let y = f 0. :: y in
+  let _Czz = einsum [ c, "ij"; c, "kj" ] "ik" * sqr inv_sigma_o in
+  let _cz_fun y = neg (einsum [ c, "ji"; y, "mi" ] "mj") * sqr inv_sigma_o in
+  let yT = List.last_exn y in
+  let gains, _, _, _ =
+    List.fold_right
+      (List.sub y ~pos:0 ~len:_T)
+      ~init:([], _cz_fun yT, _Czz, Int.(_T - 1))
+      ~f:(fun y (accu, _v, _V, t) ->
+        (* we only have state costs for t>=1 *)
+        let _cz = if t > 0 then _cz_fun y else f 0. in
+        let _Czz = if t > 0 then _Czz else f 0. in
+        let _Qzz = _Czz + einsum [ a, "ji"; _V, "jl"; a, "lk" ] "ik" in
+        let _Quz = einsum [ b, "ij"; _V, "jl"; a, "lk" ] "ki" in
+        (* or maybe ik *)
+        let _Quu = einsum [ b, "ij"; _V, "jl"; b, "kl" ] "ik" in
+        let _qz = _cz + einsum [ a, "ji"; _v, "mj" ] "mi" in
+        let _qu = einsum [ b, "ij"; _v, "mj" ] "mi" in
+        let _K = neg (solver _Quu _Quz) in
+        let _k = neg (solver _Quu _qu) in
+        let _V = _Qzz + einsum [ _Quz, "ki"; _K, "ji" ] "kj" in
+        let _v = _qz + einsum [ _qu, "mi"; _K, "ji" ] "mj" in
+        (_k, _K) :: accu, _v, _V, Int.(t - 1))
+  in
+  assert (List.length gains = _T);
+  let _k0, _ = List.hd_exn gains in
+  let u0 = _k0 in
+  let z1 = einsum [ b, "ij"; u0, "mi" ] "mj" in
+  let us, _ =
+    List.fold
+      (List.sub gains ~pos:1 ~len:Int.(_T - 1))
+      ~init:([ u0 ], z1)
+      ~f:(fun (accu, z) (_k, _K) ->
+        let u = _k + einsum [ _K, "ji"; z, "mj" ] "mi" in
+        let z = einsum [ a, "ji"; z, "mi" ] "mj" + einsum [ b, "ij"; u, "mi" ] "mj" in
+        u :: accu, z)
+  in
+  List.rev us
 
-let u_opt ~c ~inv_sigma_o y =
-  let r = Maths.(sqr inv_sigma_o) in
-  let a = Maths.(id_m + einsum [ c, "ij"; r, "j"; c, "kj" ] "ik") in
-  let b = Maths.einsum [ c, "ij"; r, "j"; y, "mj" ] "mi" in
-  solver a b
+let rollout ~a ~b ~c u =
+  let open Maths in
+  let u0 = List.hd_exn u in
+  let y_of z = einsum [ c, "ij"; z, "mi" ] "mj" in
+  let z1 = einsum [ b, "ij"; u0, "mi" ] "mj" in
+  let y, _ =
+    List.fold
+      (List.tl_exn u)
+      ~init:([ y_of z1 ], z1)
+      ~f:(fun (accu, z) u ->
+        let z' = einsum [ a, "ji"; z, "mi" ] "mj" + einsum [ b, "ij"; u, "mi" ] "mj" in
+        y_of z' :: accu, z')
+  in
+  List.rev y
+
+let sample_data =
+  let sigma_o = Maths.(exp true_theta.sigma_o_prms) in
+  fun () ->
+    let u =
+      List.init tmax ~f:(fun _ ->
+        Tensor.(randn ~device:base.device ~kind:base.kind [ bs; m ]) |> Maths.const)
+    in
+    let y =
+      rollout ~a:true_theta.a ~b:true_theta.b ~c:true_theta.c u
+      |> List.map ~f:(fun y ->
+        Maths.(y + (sigma_o * const (Tensor.randn_like (Maths.primal y)))))
+    in
+    Convenience.print [%message (List.length u : int) (List.length y : int)];
+    u, y
 
 let gaussian_llh_chol ?mu ~precision_chol:ell x =
   let d = x |> Maths.primal |> Tensor.shape |> List.last_exn in
@@ -111,11 +188,43 @@ let gaussian_llh ?mu ~inv_std x =
   let const_term = Float.(log (2. * pi) * of_int d) in
   Maths.(0.5 $* (const_term $+ error_term + cov_term)) |> Maths.neg
 
-let elbo ~data:y (theta : P.M.t) =
+(* small test *)
+let save_time_series ~out x =
+  List.map x ~f:(fun x ->
+    Maths.primal x
+    |> Tensor.to_bigarray ~kind:base.ba_kind
+    |> Owl.Mat.get_slice [ [ 0 ] ]
+    |> fun x -> Owl.Mat.reshape x [| 1; -1 |])
+  |> List.to_array
+  |> Owl.Mat.concatenate ~axis:0
+  |> Owl.Mat.save_txt ~out
+
+let u, y = sample_data ()
+let _ = save_time_series ~out:(in_dir "u") u
+let _ = save_time_series ~out:(in_dir "y") y
+
+let u_recov =
+  let inv_sigma_o = Maths.(exp (neg true_theta.sigma_o_prms)) in
+  lqr ~a:true_theta.a ~b:true_theta.b ~c:true_theta.c ~inv_sigma_o y
+
+let _ = save_time_series ~out:(in_dir "urecov") u_recov
+
+let err =
+  List.fold2_exn
+    u
+    u_recov
+    ~init:Maths.(f 0.)
+    ~f:(fun accu u u_recov -> Maths.(accu + sum (sqr (u - u_recov))))
+  |> Maths.primal
+  |> Tensor.to_float0_exn
+
+let _ = Convenience.print [%message (err : float)]
+(*
+   let elbo ~data:(y : Maths.t list) (theta : P.M.t) =
   let sigma_o = Maths.(exp theta.sigma_o_prms) in
   let inv_sigma_o = Maths.(exp (neg theta.sigma_o_prms)) in
   let inv_sigma_o_expanded = Maths.(inv_sigma_o * ones_o) in
-  let u_opt = u_opt ~c:theta.c ~inv_sigma_o:inv_sigma_o_expanded y in
+  let u_opt = lqr ~a:theta.a ~b:theta.b ~c:theta.c ~inv_sigma_o:inv_sigma_o_expanded y in
   let post_prec_chol =
     posterior_precision_chol ~c:theta.c ~inv_sigma_o:inv_sigma_o_expanded
   in
@@ -285,3 +394,4 @@ let _ =
     | _ -> failwith "-m [sofo | fgd | adam]"
   in
   optimise max_iter
+*)
