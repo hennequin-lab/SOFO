06a66681970b188435de834197ee3ac3ff084cb1
YoujingYu99 <yuyoujing@hotmail.com>
2025-05-15 15:30:49 +0100

    documentation for convenience functions

--- sofo/convenience.ml
+++ sofo/convenience.ml
@@ -3,54 +3,57 @@ open Torch
 open Forward_torch
 
 let print s = Stdio.printf "%s\n%!" (Base.Sexp.to_string_hum s)
+
+(** Transpose a along the first two dimensions. *)
 let trans_2d a = Tensor.transpose ~dim0:1 ~dim1:0 a
 
-(* extend the shape of a from [shape] to [shape; 1] *)
+(** Extend the shape of a from [shape] to [shape; 1]. *)
 let extend_one a = Maths.view a ~size:(Tensor.shape (Maths.primal a) @ [ 1 ])
 
-(* get first dimension of a tensor *)
+(** Get first dimension of a. *)
 let first_dim a = List.hd_exn (Tensor.shape a)
 
-(* efficient implementation of a *@ (transpose b) *)
+(** a *@ (transpose b) *)
 let a_b_trans a b = Tensor.einsum ~path:None [ a; b ] ~equation:"ik,jk->ij"
 
-(* efficient implementation of (transpose a) *@ b *)
+(** (transpose a) *@ b *)
 let a_trans_b a b = Tensor.einsum ~path:None [ a; b ] ~equation:"ki,kj->ij"
 
-(* int list starting from 1 and ending at the last dim of a *)
+(** Returns an int list starting from 1 and ending at the last dim of a. *)
 let all_dims_but_first a = List.range 1 (List.length (Tensor.shape a))
 
-(* sum all elements in tensor except for dimension 0. *)
+(** Sum all elements in x except for dimension 0. *)
 let sum_except_dim0 x =
   let dim = List.init (List.length (Tensor.shape x) - 1) ~f:Int.succ in
   Tensor.sum_dim_intlist x ~dim:(Some dim) ~keepdim:false ~dtype:(Tensor.type_ x)
 
-(* get top eigenvalue of x *)
+(** Get largest eigenvalue of x *)
 let top_eigval x =
   let _, s, _ = Tensor.svd ~some:true ~compute_uv:false x in
   Tensor.get_float1 s 0
 
-(* random gaussian tensor; normalised on rows *)
+(** Return a Random gaussian tensor of standard deviation [sigma] of [kind]
+ normalised on rows on [device] with shape [a;b]. *)
 let gaussian_tensor_2d_normed ~device ~kind ~a ~b ~sigma =
   let normaliser = Float.(sigma / sqrt (of_int a)) in
   Tensor.mul_scalar_ (Tensor.randn ~kind ~device [ a; b ]) (Scalar.f normaliser)
 
-(* calculate num of training loops from num of epochs desired. *)
+(** Calculate num of training loops given [full_batch_size], [batch_size] and num_epochs_to_run. *)
 let num_train_loops ~full_batch_size ~batch_size num_epochs_to_run =
   Int.(full_batch_size * num_epochs_to_run / batch_size)
 
-(* calculate current epoch number *)
+(** Calculate current epoch number given [full_batch_size], [batch_size] and iteration number. *)
 let epoch_of ~full_batch_size ~batch_size t =
   Float.(of_int t * of_int batch_size / of_int full_batch_size)
 
-(* calculate output heigh, width and channel *)
+(** Calculate output height, width and channel after a conv2d operation. *)
 let calc_out_height_width_channel
-  ~padding:(pad_x, pad_y)
-  ~dilation:(dil_x, dil_y)
-  ~in_out_channels_kernel
-  ~stride:(stride_x, stride_y)
-  ~in_height
-  ~in_width
+      ~padding:(pad_x, pad_y)
+      ~dilation:(dil_x, dil_y)
+      ~in_out_channels_kernel
+      ~stride:(stride_x, stride_y)
+      ~in_height
+      ~in_width
   =
   let _, out_channel, kerl_x, kerl_y = in_out_channels_kernel in
   let out_height =
