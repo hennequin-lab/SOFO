f89efa37a30268903dddfb9a9e68ab9ba89ccd4c
YoujingYu99 <yuyoujing@hotmail.com>
2025-05-16 16:52:37 +0100

    linear regression and Lorenz examples

--- examples/linear_regression.ml
+++ examples/linear_regression.ml
@@ -1,16 +1,16 @@
+(* Example a simple linear regression task to learn W where y = W x. *)
 open Base
 open Torch
 open Forward_torch
 open Sofo
 
+(* Command-line instruction on in which directory to save info. *)
 let in_dir = Cmdargs.in_dir "-d"
-let batch_size = 512
-let max_iter = 10_000
-let base = Optimizer.Config.Base.default
 
 (* -----------------------------------------
    -- Model setup and optimizer
    ----------------------------------------- *)
+let base = Optimizer.Config.Base.default
 
 (* Linear regression = simple one-layer neural network *)
 module One_layer = struct
@@ -18,14 +18,16 @@ module One_layer = struct
 
   type input = Tensor.t
 
+  (* output = theta *@ input. *)
   let f ~(theta : P.M.t) ~(input : input) = Maths.(theta *@ const input)
 
+  (* Initialise parameters. *)
   let init ~n ~d : P.tagged =
     let sigma = Float.(1. / sqrt (of_int n)) in
     Tensor.(f sigma * randn ~kind:base.kind ~device:base.device [ d; n ]) |> Prms.free
 end
 
-(* Feedforward model wrapper with MSE loss *)
+(* Feedforward model with MSE loss *)
 module FF =
   Wrapper.Feedforward
     (One_layer)
@@ -33,7 +35,8 @@ module FF =
          let scaling_factor = 1.
        end))
 
-(* Optimiser; here you can switch to Adam to compare. *)
+(* Instatiate optimiser. *)
+(* Sofo configuration with learning rate, number of tangents and damping specified. *)
 module O = Optimizer.SOFO (FF)
 
 let config =
@@ -49,11 +52,13 @@ let config =
 (* -----------------------------------------
    -- Generate linear regression data.
    ----------------------------------------- *)
-
+(* Dimension of x. *)
 let n = 100
+
+(* Dimension of y. *)
 let d = 1
 
-(* input covariance ( = Fisher information matrix in this case) *)
+(* Input covariance ( = Fisher information matrix in this case). *)
 let input_cov12 =
   let lambda =
     Array.init n ~f:(fun i -> Float.(1. / (1. + square (of_int Int.(i + 1)))))
@@ -65,28 +70,36 @@ let input_cov12 =
   in
   Tensor.(u * sqrt lambda)
 
-(* true weights *)
+(* True weights. *)
 let teacher : One_layer.P.M.t =
   One_layer.init ~n ~d |> One_layer.P.value |> One_layer.P.const
 
-(* data for mini batch. *)
+(* Generate data for mini batch. *)
 let minibatch bs =
   let x = Tensor.(matmul input_cov12 (randn ~device:base.device [ n; bs ])) in
   let y = One_layer.f ~theta:teacher ~input:x |> Maths.primal in
   x, y
 
-(* optimization loop *)
+(* -----------------------------------------
+   -- Optimization loop       ------
+   ----------------------------------------- *)
+let batch_size = 512
+let max_iter = 10_000
+
 let rec loop ~t ~out ~state =
   Stdlib.Gc.major ();
+  (* Generate data. *)
   let data = minibatch batch_size in
+  (* Take one optimization step to update state and returns loss. *)
   let loss, new_state = O.step ~config ~state ~data ~args:() in
   if t % 100 = 0
   then (
     Convenience.print [%message (t : int) (loss : float)];
+    (* Save loss information in a text file. *)
     Owl.Mat.(save_txt ~append:true ~out (of_array [| Float.of_int t; loss |] 1 2)));
   if t < max_iter then loop ~t:(t + 1) ~out ~state:new_state
 
-(* start the loop *)
+(* Start the loop. *)
 let _ =
   let out = in_dir "loss" in
   Bos.Cmd.(v "rm" % "-f" % out) |> Bos.OS.Cmd.run |> ignore;
--- examples/lorenz.ml
+++ examples/lorenz.ml
@@ -1,3 +1,4 @@
+(* Example on training a vanilla RNN to learn the chaotic Lorenz attractor. *)
 open Printf
 open Base
 open Owl
@@ -7,27 +8,21 @@ open Sofo
 module Arr = Dense.Ndarray.S
 module Mat = Dense.Matrix.S
 
+(* Setting seed. *)
 let _ =
-  (* Random.init 1999; *)
   Random.self_init ();
   Owl_stats_prng.init (Random.int 100000);
   Torch_core.Wrapper.manual_seed (Random.int 100000)
 
+(* Command-line instruction on in which directory to save info. *)
 let in_dir = Cmdargs.in_dir "-d"
-let _ = Bos.Cmd.(v "rm" % "-f" % in_dir "info") |> Bos.OS.Cmd.run
+
+(* -----------------------------------------
+   -- Model setup and optimizer
+   ----------------------------------------- *)
 let base = Optimizer.Config.Base.default
 
-let config ~base_lr ~gamma =
-  Optimizer.Config.SOFO.
-    { base
-    ; learning_rate = Some base_lr
-    ; n_tangents = 128
-    ; rank_one = false
-    ; damping = gamma
-    ; momentum = None
-    }
-
-(* neural network *)
+(* Neural network. *)
 module RNN = struct
   module PP = struct
     type 'a p =
@@ -43,9 +38,11 @@ module RNN = struct
 
   type input = unit
 
+  (* One-step forward function for the RNN. [theta] is the parameters and y is the network state. *)
   let f ~(theta : P.M.t) ~input:_ y =
     Maths.((y *@ theta.a) + (relu (theta.b + (y *@ theta.c)) *@ theta.w))
 
+  (* Initialise parameters. *)
   let init ~d ~dh : P.tagged =
     let w =
       Convenience.gaussian_tensor_2d_normed
@@ -68,15 +65,9 @@ module RNN = struct
       Tensor.(mul_scalar (eye ~n:d ~options:(base.kind, base.device)) (Scalar.f 0.9))
     in
     { w; c; b; a = Prms.free a }
-
-  let simulate ~(theta : P.M.t) ~horizon y0 =
-    let rec iter t accu y =
-      if t = 0 then List.rev accu else iter (t - 1) (y :: accu) (f ~theta ~input:() y)
-    in
-    iter horizon [] (Maths.const y0)
 end
 
-(* feedforward model with mse loss *)
+(* Feedforward model with MSE loss *)
 module FF =
   Wrapper.Recurrent
     (RNN)
@@ -84,7 +75,18 @@ module FF =
          let scaling_factor = 1.
        end))
 
-(* optimiser *)
+(* Instatiate optimiser. *)
+(* Sofo configuration with learning rate, number of tangents and damping specified. *)
+let config =
+  Optimizer.Config.SOFO.
+    { base
+    ; learning_rate = Some 0.01
+    ; n_tangents = 128
+    ; rank_one = false
+    ; damping = Some 1e-5
+    ; momentum = None
+    }
+
 module O = Optimizer.SOFO (FF)
 
 (* -----------------------------------------
@@ -93,7 +95,10 @@ module O = Optimizer.SOFO (FF)
 
 open Lorenz_common
 
+(* Lorenz state dimension, which is the same as the RNN state dimension. *)
 let d = 3
+
+(* Inverted bottleneck dimension. *)
 let dh = 400
 let batch_size = 256
 let num_epochs_to_run = 2000
@@ -102,97 +107,52 @@ let train_data = data 32
 let full_batch_size = Arr.(shape train_data).(1)
 let train_data_batch = get_batch train_data
 let test_horizon = 10000
-let full_batch_size = Arr.(shape train_data).(1)
-let _ = Convenience.print [%message (full_batch_size : int)]
-let train_data_batch = get_batch train_data
 
 let num_train_loops =
   Convenience.num_train_loops ~full_batch_size ~batch_size num_epochs_to_run
 
 let epoch_of t = Convenience.epoch_of ~full_batch_size ~batch_size t
 
-(* simulate n trials from saved parameters; first 3 columns are predictions and last 3 columns are ground truth *)
-let simulate ~f_name n_trials =
-  let model_params =
-    let params_ba = O.W.P.T.load (in_dir f_name ^ "_params") in
-    RNN.P.map params_ba ~f:(fun x -> x |> Maths.const)
-  in
-  let n_list = List.range 0 n_trials in
-  List.iter n_list ~f:(fun j ->
-    (* ground truth obtained from integration *)
-    let y_true = data_test (test_horizon - 1) in
-    (* use same initial condition to simulate with model *)
-    let init_cond_sim = Mat.get_slice [ [ 0 ]; [] ] y_true in
-    let simulated_arr =
-      RNN.simulate
-        ~theta:model_params
-        ~horizon:test_horizon
-        Tensor.(of_bigarray ~device:base.device init_cond_sim)
-      |> List.map ~f:(fun yt ->
-        let yt = Maths.primal yt in
-        let yt = Tensor.to_bigarray ~kind:base.ba_kind yt in
-        Arr.expand yt 3)
-      |> Array.of_list
-      |> Arr.concatenate ~axis:0
-      |> Arr.transpose ~axis:[| 1; 0; 2 |]
-    in
-    simulated_arr
-    |> Arr.iter_slice ~axis:0 (fun yi ->
-      let yi = Arr.squeeze yi in
-      let yi_tot = Mat.concat_horizontal yi y_true in
-      Mat.save_txt ~out:(in_dir (sprintf "%s_autonomous%i" f_name j)) yi_tot))
-
-let rec iter ~f_name ~config ~t ~state ~time_elapsed running_avg =
+(* -----------------------------------------
+   -- Optimization loop       ------
+   ----------------------------------------- *)
+let rec iter ~f_name ~config ~t ~state running_avg =
   Stdlib.Gc.full_major ();
   let e = epoch_of t in
+  (* Get initial condition and the full trajectory as data. *)
   let init_cond, data =
     let trajectory = train_data_batch batch_size in
     let init_cond = List.hd_exn trajectory in
     ( Tensor.of_bigarray ~device:base.device init_cond
-    , List.mapi trajectory ~f:(fun tt x ->
-        (* only label provided is the end point *)
-        if tt = 31
-        then (
-          let x = Tensor.of_bigarray ~device:base.device x in
-          (), Some x)
-        else (), None) )
+    , List.map trajectory ~f:(fun x ->
+        let x = Tensor.of_bigarray ~device:base.device x in
+        (), Some x) )
   in
-  let t0 = Unix.gettimeofday () in
+  (* Take one optimization step to update state and returns loss. *)
   let loss, new_state = O.step ~config ~state ~data ~args:init_cond in
-  let t1 = Unix.gettimeofday () in
-  let time_elapsed = Float.(time_elapsed + t1 - t0) in
   let running_avg =
     if t % 10 = 0
     then (
-      (* save params *)
-      O.W.P.T.save
-        (RNN.P.value (O.params new_state))
-        ~kind:base.ba_kind
-        ~out:(in_dir f_name ^ "_params");
-      (* if t % 500 = 0 then simulate ~f_name n_trials_simulation; *)
       let loss_avg =
         match running_avg with
         | [] -> loss
         | running_avg -> running_avg |> Array.of_list |> Owl.Stats.mean
       in
       Convenience.print [%message (e : float) (loss_avg : float)];
+      (* Save loss information in a text file. *)
       Mat.(
         save_txt
           ~append:true
           ~out:(in_dir f_name)
-          (of_array [| epoch_of t; time_elapsed; loss_avg |] 1 3));
+          (of_array [| epoch_of t; loss_avg |] 1 3));
       [])
     else running_avg
   in
   if t < num_train_loops
-  then iter ~f_name ~config ~t:(t + 1) ~state:new_state ~time_elapsed (loss :: running_avg)
+  then iter ~f_name ~config ~t:(t + 1) ~state:new_state (loss :: running_avg)
 
-let lr_rate = 0.01
-let damping = Some 1e-5
-let meth = "ggn"
-
+(* Start the loop. *)
 let _ =
-  let config = config ~base_lr:0.01 ~gamma:(Some 1e-05) in
   let f_name = "lorenz" in
   Bos.Cmd.(v "rm" % "-f" % in_dir f_name) |> Bos.OS.Cmd.run |> ignore;
-  iter ~f_name ~config ~t:0 ~state:(O.init ~config RNN.(init ~d ~dh)) ~time_elapsed:0. []
+  iter ~f_name ~config ~t:0 ~state:(O.init ~config RNN.(init ~d ~dh)) []
