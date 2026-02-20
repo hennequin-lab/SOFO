03360ce641273fa399ae08cf51939b77d8f0dc2e
hennequin <gje.hennequin@gmail.com>
2026-02-20 13:54:47 +0000

    updated tests

--- tests/framework/framework.ml
+++ tests/framework/framework.ml
@@ -5,21 +5,21 @@ open Maths
 let print s = Stdio.print_endline (Sexp.to_string_hum s)
 
 module Make (P : Prms.T) (O : Prms.T) = struct
-  let run (p : const P.t) ~(f : [< `const | `dual ] P.t -> any O.t) =
+  let run (p : P.t) ~(f : P.t -> O.t) =
     let open Torch in
     (* batched JVP tests *)
     let k = 7 in
     let dot_prod_1, v, w =
       (* samples v where it has one extra dimension in front of size k *)
       let v = P.randn_like_k ~k p in
-      let p = P.dual p ~tangent:v in
-      let o = f (P.any p) in
+      let p = P.dual p ~tangent:(P.primal v) in
+      let o = f p in
       let w = O.randn_like o in
       let o = O.tangent_exn o in
       let dp1 =
         O.fold2 w o ~init:(Maths.f 0.) ~f:(fun accu (w, o, _) ->
-          let wo = C.(reshape (w * o) ~shape:[ k; -1 ]) in
-          C.(accu + sum ~dim:[ 1 ] ~keepdim:false wo))
+          let wo = reshape (w * const o) ~shape:[ k; -1 ] in
+          accu + sum ~dim:[ 1 ] ~keepdim:false wo)
       in
       dp1, v, w
     in
@@ -27,30 +27,30 @@ module Make (P : Prms.T) (O : Prms.T) = struct
     let dot_prod_2 =
       let p =
         P.map p ~f:(fun x ->
-          let x = to_tensor x in
+          let x = primal x in
           let x = Tensor.set_requires_grad (Tensor.copy x) ~r:true in
           Tensor.zero_grad x;
-          Maths.of_tensor x)
+          const x)
       in
       (* make sure that every single input parameter has a zero gradient *)
       let _ =
-        let dummy = P.C.dot_prod p p in
-        Tensor.backward (to_tensor dummy);
-        P.iter p ~f:(fun x -> Tensor.zero_grad (to_tensor x))
+        let dummy = P.dot_prod p p in
+        Tensor.backward (primal dummy);
+        P.iter p ~f:(fun x -> Tensor.zero_grad (primal x))
       in
-      let o = f (P.any p) in
-      let surrogate = O.C.dot_prod w (O.const o) in
-      Tensor.backward (to_tensor surrogate);
-      let g = P.map p ~f:(fun x -> of_tensor (Tensor.grad (to_tensor x))) in
+      let o = f p in
+      let surrogate = O.dot_prod w o in
+      Tensor.backward (primal surrogate);
+      let g = P.map p ~f:(fun x -> const (Tensor.grad (primal x))) in
       let dp2 =
         P.fold2 v g ~init:(Maths.f 0.) ~f:(fun accu (v, g, _) ->
-          let vg = C.(reshape (v * g) ~shape:[ k; -1 ]) in
-          C.(accu + sum ~dim:[ 1 ] ~keepdim:false vg))
+          let vg = reshape (v * g) ~shape:[ k; -1 ] in
+          accu + sum ~dim:[ 1 ] ~keepdim:false vg)
       in
       dp2
     in
     let err =
-      C.(mean (sqr (dot_prod_1 - dot_prod_2) / mean (sqr (dot_prod_1 + dot_prod_2))))
+      mean (sqr (dot_prod_1 - dot_prod_2) / mean (sqr (dot_prod_1 + dot_prod_2)))
       |> to_float_exn
     in
     dot_prod_1, dot_prod_2, err
--- tests/test_maths.ml
+++ tests/test_maths.ml
@@ -35,10 +35,8 @@ type input_constr =
 (* each unary test test is characterized by a name,
    a (potentially empty) list of constraints on the input,
    and a unary math function to be tested *)
-type 'a unary = string * input_constr list * (int list -> 'a some t -> 'a t)
-
-type ('a, 'b) binary =
-  string * input_constr list * (int list -> 'a some t -> 'b some t -> any t)
+type unary = string * input_constr list * (int list -> t -> t)
+type binary = string * input_constr list * (int list -> t -> t -> t)
 
 let any_shape f _ x = f x
 
@@ -77,7 +75,7 @@ let check_grad2 f x y =
   e
 
 (* this is how we test a unary function *)
-let test_unary ((name, input_constr, f) : _ unary) =
+let test_unary ((name, input_constr, f) : unary) =
   ( name
   , `Quick
   , fun () ->
@@ -120,22 +118,19 @@ let test_unary ((name, input_constr, f) : _ unary) =
         in
         let f = f shape in
         let x = generate_tensor ~shape ~input_constr_list:input_constr in
-        Alcotest.(check @@ rel_tol) name 0.0 (check_grad1 f (of_tensor x))) )
+        Alcotest.(check @@ rel_tol) name 0.0 (check_grad1 f (const x))) )
 
-let inv_sqr x =
-  let x_primal = Maths.to_tensor x in
+let inv x =
+  let x_primal = primal x in
   let x_device = Tensor.device x_primal in
-  let x_kind = Tensor.type_ x_primal in
   (* make sure x is positive definite *)
-  let x_sym = Maths.(x *@ btr x) in
+  let x_sym = x *@ btr x in
   let x_size = List.last_exn (Tensor.shape x_primal) in
-  let x_final =
-    Maths.(x_sym + of_tensor (Tensor.eye ~n:x_size ~options:(x_kind, x_device)))
-  in
-  Maths.inv_sqr x_final
+  let x_final = x_sym + eye x_size ~device:x_device in
+  inv x_final
 
 let unary_tests =
-  let test_list : _ unary list =
+  let test_list : unary list =
     [ ( "permute"
       , [ `specified_unary [ 2; 3; 6; 4 ] ]
       , any_shape (permute ~dims:[ 0; 2; 3; 1 ]) )
@@ -180,7 +175,7 @@ let unary_tests =
     ; "tanh", [], any_shape tanh
     ; "pdf", [], any_shape pdf (* ; "cdf", [], any_shape cdf *)
     ; "erf", [], any_shape erf
-    ; "inv_sqr", [ `specified_unary [ 4; 4 ] ], any_shape inv_sqr
+    ; "inv", [ `specified_unary [ 4; 4 ] ], any_shape inv
     ; "relu", [ `positive ], any_shape relu
     ; "soft_relu", [], any_shape soft_relu
     ; ( "sum"
@@ -231,7 +226,7 @@ let unary_tests =
       , fun shape ->
           let n_dims = List.length shape in
           let keepdim = Random.bool () in
-          let dim = Int.(Random.int n_dims) in
+          let dim = Random.int n_dims in
           max ~dim ~keepdim )
     ; ( "transpose"
       , [ `order_greater_than 2 ]
@@ -295,7 +290,7 @@ let random_linsolve_2d_matrix_shapes () =
 
 (* this is how we test a binary function *)
 (* for simplicity apply same constraint on both tensors. *)
-let test_binary ((name, input_constr, f) : (_, _) binary) =
+let test_binary ((name, input_constr, f) : binary) =
   ( name
   , `Quick
   , fun () ->
@@ -372,51 +367,40 @@ let test_binary ((name, input_constr, f) : (_, _) binary) =
         Alcotest.(check @@ rel_tol)
           name
           0.0
-          (check_grad2 (fun (x, y) -> f x y) (of_tensor x) (of_tensor y))) )
+          (check_grad2 (fun (x, y) -> f x y) (const x) (const y))) )
 
-let matmul_with_einsum a b = Maths.einsum [ a, "ij"; b, "jk" ] "ik"
-let batch_matmul_with_einsum_33 a b = Maths.einsum [ a, "mij"; b, "mjk" ] "mik"
-let batch_matmul_with_einsum_32 a b = Maths.einsum [ a, "mij"; b, "mj" ] "mi"
-let batch_trans_matmul_with_einsum a b = Maths.(einsum [ a, "mij"; b, "mik" ] "mjk")
-let batch_vecmat_with_einsum a b = Maths.(einsum [ a, "mi"; b, "mij" ] "mj")
-let batch_vecmat_trans_with_einsum a b = Maths.(einsum [ a, "mi"; b, "mji" ] "mj")
+let matmul_with_einsum a b = einsum [ a, "ij"; b, "jk" ] "ik"
+let batch_matmul_with_einsum_33 a b = einsum [ a, "mij"; b, "mjk" ] "mik"
+let batch_matmul_with_einsum_32 a b = einsum [ a, "mij"; b, "mj" ] "mi"
+let batch_trans_matmul_with_einsum a b = einsum [ a, "mij"; b, "mik" ] "mjk"
+let batch_vecmat_with_einsum a b = einsum [ a, "mi"; b, "mij" ] "mj"
+let batch_vecmat_trans_with_einsum a b = einsum [ a, "mi"; b, "mji" ] "mj"
 
 let linsolve ~left a b =
-  let a_primal = Maths.to_tensor a in
+  let a_primal = primal a in
   let a_device = Tensor.device a_primal in
-  let a_kind = Tensor.type_ a_primal in
   let n = List.last_exn (Tensor.shape a_primal) in
   (* improve condition number of a *)
-  let a =
-    Maths.(
-      of_tensor
-        Tensor.(
-          a_primal
-          + Tensor.(
-              mul_scalar
-                (eye ~n ~options:(a_kind, a_device))
-                (Scalar.f Float.(10. *. of_int n)))))
-  in
-  Maths.linsolve ~left a b
+  let a = const a_primal + (eye n ~device:a_device *$ Float.(10. *. of_int n)) in
+  linsolve ~left a b
 
 let linsolve_tri ~left ~upper a b =
   let a =
     match upper with
-    | false -> Maths.tril a ~_diagonal:0
-    | true -> Maths.(btr (tril a ~_diagonal:0))
+    | false -> tril a ~_diagonal:0
+    | true -> btr (tril a ~_diagonal:0)
   in
-  Maths.linsolve_triangular a b ~left ~upper
+  linsolve_triangular a b ~left ~upper
 
-let concat_list a b = Maths.concat [ a; b ] ~dim:0
-let block_diag a b = Maths.block_diag [ a; b ]
+let concat_list a b = concat [ a; b ] ~dim:0
 
 let binary_tests =
-  let test_list : (_, _) binary list =
-    [ "plus", [], any_shape Maths.( + )
-    ; "minus", [], any_shape Maths.( - )
-    ; "mult", [], any_shape Maths.( * )
-    ; "div", [ `positive ], any_shape Maths.( / )
-    ; "matmul", [ `matmul ], any_shape Maths.( *@ )
+  let test_list : binary list =
+    [ "plus", [], any_shape ( + )
+    ; "minus", [], any_shape ( - )
+    ; "mult", [], any_shape ( * )
+    ; "div", [ `positive ], any_shape ( / )
+    ; "matmul", [ `matmul ], any_shape ( *@ )
     ; "matmul_with_einsum", [ `matmul ], any_shape matmul_with_einsum
     ; ( "batch_matmul_with_einsum_33"
       , [ `specified_binary ([ 3; 4; 5 ], [ 3; 5; 7 ]) ]
@@ -447,22 +431,8 @@ let binary_tests =
     ; ( "linsolve_tri_left_false_upper_false"
       , [ `linsolve_left_false ]
       , any_shape (linsolve_tri ~left:false ~upper:false) )
-    ; "kron", [ `specified_binary ([ 3; 4 ], [ 5; 7 ]) ], any_shape Maths.kron
+    ; "kron", [ `specified_binary ([ 3; 4 ], [ 5; 7 ]) ], any_shape kron
     ; "concat", [], any_shape concat_list
-    ; "block_diag", [ `order_equal_to 2 ], any_shape block_diag
-    ; ( "conv2d"
-      , [ `order_equal_to 4 ]
-      , fun _ ->
-          let stride = Int.(1 + Random.int 1) in
-          fun w x ->
-            Maths.conv2d
-              ~padding:(0, 0)
-              ~dilation:(1, 1)
-              ~groups:1
-              ~bias:None
-              ~stride:(stride, stride)
-              ~w
-              x )
     ]
   in
   List.map ~f:test_binary test_list
